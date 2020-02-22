ca1690206003af0ff8e493d2066eca4fb92bf839
Dominic Michael Silveira <dms24081999@gmail.com>
2020-02-22 12:34:35 +0530

    Added support for test set file and download predicted values with accuracy

--- src/dataminingcore/views.py
+++ src/dataminingcore/views.py
@@ -8,10 +8,10 @@ from django.http import HttpResponse
 
 class NaiveBayes(APIView):
     def post(self, request):
-        csv_file = request.FILES['file']
+        csv_file = request.FILES.get('training_set')
         if not csv_file.name.endswith('.csv'):
-            return Response({"failed":"12"})
-        data = pd.read_csv(request.FILES.get('file'))
+            return Response({"failed":"Traing Set should be a CSV file!"})
+        data = pd.read_csv(csv_file)
         if request.POST.get('id_indices'):
             print(json.loads(request.POST.get('id_indices')))
             data=data.drop(json.loads(request.POST.get('id_indices')), axis=1)
@@ -20,30 +20,28 @@ class NaiveBayes(APIView):
         column_names=[columnName for (columnName, columnData) in data.iteritems()][:-1]
         last_column_name=[columnName for (columnName, columnData) in data.iteritems()][-1]
 
-        stat_str = request.data['stat']
-        stat_dict = json.loads(stat_str)
-        stat = pd.DataFrame(stat_dict) 
-        stat=stat.assign(predicted='')
+        csv_file = request.FILES.get('test_set')
+        if not csv_file.name.endswith('.csv'):
+            return Response({"failed":"Test Set should be a CSV file!"})
+        stat = pd.read_csv(csv_file)
+        stat=stat.assign(predicted='',accuracy=float())
         print(stat)
 
         n=dict()
         for uniq in data[last_column_name].unique():
             n[uniq]=data[last_column_name][data[last_column_name] == uniq].count()
-
         total_stat = data[last_column_name].count()
-
         P=dict()
         for uniq in data[last_column_name].unique():
             P[uniq]=n[uniq]/total_stat
-
         n_counts=dict()
         P_array=dict()
 
         #------------------------For Multiple Data----------------------------------
         response=dict()
-        for i in range(stat.shape[0]):
+        for i in range(stat.shape[0]):     
             for uniq in data[last_column_name].unique():
-                n_counts[uniq]=[]
+                n_counts[uniq]=[] 
                 P_array[uniq]=[]
             for column_name in column_names: 
                 for uniq in data[last_column_name].unique():
@@ -56,11 +54,15 @@ class NaiveBayes(APIView):
             print("X belongs to '"+str(won)+"' class: "+str(multiply_array_P[won])) 
             response["data "+str(i)]="X belongs to '"+str(won)+"' class: "+str(multiply_array_P[won]) 
             stat.at[i,'predicted']=won
+            stat.at[i,'accuracy']=multiply_array_P[won]
         print(stat)
-        # response = HttpResponse(content_type='text/csv')
-        # response['Content-Disposition'] = 'attachment; filename=filename.csv'
-        # stat.to_csv(path_or_buf=response,sep=',',float_format='%.2f',index=False,decimal=",")
-        # return response
+
+        if request.POST.get('output_format') is not None and str(json.loads(request.POST.get('output_format')))==str("file"):
+            filename=request.FILES.get('test_set').name+"-predicted.csv"
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename='+filename
+            stat.to_csv(path_or_buf=response,sep=',',index=False) 
+            return response
         return Response(response) 
         #------------------------For Single Data----------------------------------
         # for uniq in data[last_column_name].unique():
